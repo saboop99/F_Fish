dbad30c5063670f05d513f8d5f7deca95a368388
Laboratório de Informática <rafa4021@hotmail.com>
2023-05-26 21:48:39 -0300

    add FloxingManager script

---
+++ Assets/Scripts/FlockingManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingManager : MonoBehaviour
+{
+
+    public GameObject fishPrefab;
+    public int numFish = 20;
+    public GameObject[] allFish;
+    public Vector3 swinLimits = new Vector3(5, 5, 5);
+
+    [Header("Configurações do Cardume")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x),
+                                                                Random.Range(-swinLimits.y, swinLimits.y),
+                                                                Random.Range(-swinLimits.z, swinLimits.z));
+            allFish[1] = (GameObject) Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
