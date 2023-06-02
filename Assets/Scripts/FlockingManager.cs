53bb3ee7d4e5c69bde3d6c140a18a5b7a56d5ba1
saboop99 <rafa4021@hotmail.com>
2023-06-02 10:47:09 -0300

    att dos scripts Flock e FlockingManager

--- Assets/Scripts/FlockingManager.cs
+++ Assets/Scripts/FlockingManager.cs
@@ -1,35 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class FlockingManager : MonoBehaviour
 {
-
     public GameObject fishPrefab;
     public int numFish = 20;
     public GameObject[] allFish;
     public Vector3 swinLimits = new Vector3(5, 5, 5);
-
+    public Vector3 goalPos;
     [Header("Configurações do Cardume")]
     [Range(0.0f, 5.0f)]
     public float minSpeed;
     [Range(0.0f, 5.0f)]
     public float maxSpeed;
-
+    [Range(1.0f, 10.0f)]
+    public float neighbourDistance;
+    [Range(0.0f, 5.0f)]
+    public float rotationSpeed;
     void Start()
     {
         allFish = new GameObject[numFish];
         for (int i = 0; i < numFish; i++)
         {
-            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x),
-                                                                Random.Range(-swinLimits.y, swinLimits.y),
-                                                                Random.Range(-swinLimits.z, swinLimits.z));
-            allFish[1] = (GameObject) Instantiate(fishPrefab, pos, Quaternion.identity);
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x,
+            swinLimits.x),
+            Random.Range(-swinLimits.y,
+            swinLimits.y),
+            Random.Range(-swinLimits.z,
+            swinLimits.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
         }
+        goalPos = this.transform.position;
     }
-
+    // Update is called once per frame
     void Update()
     {
-        
+        goalPos = this.transform.position;
+        if (Random.Range(0, 100) < 10)
+            goalPos = this.transform.position + new Vector3(Random.Range(-swinLimits.x,
+            swinLimits.x),
+            Random.Range(-swinLimits.y,
+            swinLimits.y),
+            Random.Range(-swinLimits.z,
+            swinLimits.z));
     }
 }
