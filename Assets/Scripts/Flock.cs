86bf25938524928701dee85cb792fcb7206bbc04
saboop99 <rafa4021@hotmail.com>
2023-06-09 23:01:06 -0300

    comecei a comentar o código flock

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Flock : MonoBehaviour
 {
+    //criando variavel manager
     public FlockingManager myManager;
+    //criando variavel speed
     public float speed;
+    //criando variavel bool 
     bool turning = false;
-    // Start is called before the first frame update
+    //definindo speed como min e max
     void Start()
     {
         speed = Random.Range(myManager.minSpeed,
@@ -14,27 +17,36 @@ public class Flock : MonoBehaviour
     }
     void Update()
     {
+        //definindo "b" como Bounds, e o limite que o cardume pode nadar
         Bounds b = new Bounds(myManager.transform.position, myManager.swinLimits * 2);
+        //definindo "hit" como um novo RaycastHit
         RaycastHit hit = new RaycastHit();
+        //definindo o valor da variavel "direction"
         Vector3 direction = myManager.transform.position - transform.position;
+
+        //condicional para fazer o peixe virar
         if (!b.Contains(transform.position))
         {
             turning = true;
             direction = myManager.transform.position - transform.position;
         }
+        //condicional para o peixe virar caso o Raycast acerte alguma coisa
         else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit))
         {
             turning = true;
             direction = Vector3.Reflect(this.transform.forward, hit.normal);
         }
+        //utilização do else para caso nenhuma das condicionais acima de concretize, então "turning" será falso
         else
             turning = false;
+        //condicional que define o as propriedades de "turning", rotação
         if (turning)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation,
             Quaternion.LookRotation(direction),
             myManager.rotationSpeed * Time.deltaTime);
         }
+        //se não for "turning", define a speed como um valor aleatório entre 0 e 100/
         else
         {
             if (Random.Range(0, 100) < 10)
